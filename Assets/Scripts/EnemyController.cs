8de7fbf07b4b448d2294bd36f8f29edf575f814e
aUsername0704 <luka.cigic7@gmail.com>
2025-03-06 20:35:57 +0100

    Improved enemy npc movement to be switching movement axis

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -12,13 +12,15 @@ public class EnemyController : MonoBehaviour
     float timer; //vrijeme koje odbrojava do iduce promjene smjera
     int direction = 1; //varijabla koja odredjuje smjer kretanja, pozitivno je naprijed po osi, negativno je unazad
 
-
+    public float promjenaOsi = 5.0f;
+    float timerOsi;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         timer = changeTime; //pocetak timera ce biti change Time varijabla jer je pocetak i nije se jos nista promjenilo
+        timerOsi = promjenaOsi;
           
     }
 
@@ -26,12 +28,20 @@ public class EnemyController : MonoBehaviour
     void Update()
     {
         timer -= Time.deltaTime;
+        timerOsi -= Time.deltaTime;
 
+        
         if(timer <= 0)
         {
             direction = -direction; //ako je timer manji ili jednak 0, promjeni smjer, dva - daje pozitivno, jedan ce napravit negativno
             timer = changeTime; //resetira timer
 
+        }
+
+        if (timerOsi <= 0) {
+
+            vertical = !vertical;
+            timerOsi = promjenaOsi;
         }
 
         
@@ -40,18 +50,22 @@ public class EnemyController : MonoBehaviour
     void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position; //postion sprema poziciju rigidbodya
+      
+            if (vertical) //ako je vertical true
+            {
+                position.y = position.y + EnemySpeed * direction * Time.deltaTime; //pomak po y osi
 
-        if (vertical) //ako je vertical true
-        {
-            position.y = position.y + EnemySpeed * direction  * Time.deltaTime; //pomak po y osi
-        }
-        else
-        {
-            position.x = position.x + EnemySpeed * direction * Time.deltaTime; //pomak po x osi
-        }
-        
-        rigidbody2d.MovePosition(position); //pomak rigidbodya na novu poziciju
-    }
+            }
+            else
+            {
+                position.x = position.x + EnemySpeed * direction * Time.deltaTime; //pomak po x osi
+
+            }
+
+            rigidbody2d.MovePosition(position); //pomak rigidbodya na novu poziciju
+        }   
+
+
 
     void OnTriggerEnter2D(Collider2D other)
     {
