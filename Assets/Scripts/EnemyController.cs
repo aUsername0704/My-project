72470ede5e52ce5f1809fa7166684e9e5952fc96
aUsername0704 <luka.cigic7@gmail.com>
2025-03-08 17:11:20 +0100

    Added Enemy walking animations.

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 
 public class EnemyController : MonoBehaviour
 {
+    Animator animator;
     public float EnemySpeed = 5.0f;
     Rigidbody2D rigidbody2d;
     public bool vertical;
@@ -18,6 +19,7 @@ public class EnemyController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        animator = GetComponent<Animator>();
         rigidbody2d = GetComponent<Rigidbody2D>();
         timer = changeTime; //pocetak timera ce biti change Time varijabla jer je pocetak i nije se jos nista promjenilo
         timerOsi = promjenaOsi;
@@ -54,13 +56,17 @@ public class EnemyController : MonoBehaviour
             if (vertical) //ako je vertical true
             {
                 position.y = position.y + EnemySpeed * direction * Time.deltaTime; //pomak po y osi
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
 
             }
             else
             {
                 position.x = position.x + EnemySpeed * direction * Time.deltaTime; //pomak po x osi
+                animator.SetFloat("Move X", direction);
+                animator.SetFloat("Move Y", 0);
 
-            }
+        }
 
             rigidbody2d.MovePosition(position); //pomak rigidbodya na novu poziciju
         }   
